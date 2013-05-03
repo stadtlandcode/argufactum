bfcfaa67cf1dd5d3a2965f6127d04976a6caf6b5
Felix Ebert <felix.ebert@if-core.de>
2013-05-03 10:39:50 +0200

    questionnaire index page improved

--- static/web/js/questionnaire/examples/rbs.js
+++ static/web/js/questionnaire/examples/rbs.js
@@ -7,10 +7,14 @@
 				number: 1,
 				text: 'Sind Sie der Meinung, dass die Eltern mit dem Wegfall der Realschule RBS eine zu kleine Wahlmöglichkeit zwischen allen Schulformen haben?',
 				supports: 'YES',
-				facts: [{
-					number: 1,
-					text: 'Die Eltern hätten nach Wegfall der Realschule RBS noch die Wahl zwischen 1 Realschule, 3 Gymnasien, 2 Gesamtschulen und 1 Werkrealschule in Grevenbroich'
-				}]
+				facts: [
+					{
+						number: 1,
+						text: 'Die Eltern hätten nach Wegfall der Realschule RBS noch die Wahl zwischen 1 Realschule, 3 Gymnasien, 2 Gesamtschulen und 1 Werkrealschule in Grevenbroich'
+					}, {
+						number: 2,
+						text: 'Grevenbroich hat ca. 64.000 Einwohner'
+					}]
 			},
 			{
 				number: 2,
@@ -48,6 +52,38 @@
 					{
 						number: 2,
 						text: 'Die Eltern befürchten, dass spätestens nach 2-3 Jahren kaum noch qualifizierte Lehrer für alle Unterrichtsfächer vorhanden sein werden und mit der Zeit ein solcher Notstand eintritt, dass viele Unterrichtsstunden ausfallen und beispielsweise ein Sportlehrer Englischunterricht geben muss.'
+					}]
+			},
+			{
+				number: 5,
+				text: 'Halten Sie die Mehrkosten für den Erhalt der Realschule RBS für vertretbar?',
+				supports: 'YES',
+				facts: [{
+					number: 1,
+					text: 'Für den Erhalt der Realschule fallen Mehrkosten von 400.000 Euro verteilt über 5 Jahre an'
+				}]
+			},
+			{
+				number: 6,
+				text: 'Sind Sie dafür, dass die Stadt weitere Optionen als die Auflösung der Realschule RBS prüfen muss?',
+				supports: 'YES',
+				facts: [{
+					number: 1,
+					text: 'Die Stadt hat laut den Eltern bisher nur die Option zur Auflösung der Realschule RBS geprüft. Alternativen sollen dabei keine Rolle gespielt haben.'
+				}]
+			},
+			{
+				number: 7,
+				text: 'Halten Sie die Realschule RBS für besonders erhaltenswert?',
+				supports: 'YES',
+				facts: [
+					{
+						number: 1,
+						text: 'Die Realschule RBS wurde im Sommer 2012 von Ministerpräsidentin Hannelore Kraft als eine der herausragenden Schulen in NRW ausgezeichnet'
+					},
+					{
+						number: 2,
+						text: 'Das Förderkonzept zur individuellen Förderung sowie die Profilklassen sichern mit einem bewährten und verlässlichen Angebot erfolgreiche Schulabschlüsse.'
 					}]
 			}]
 	};
